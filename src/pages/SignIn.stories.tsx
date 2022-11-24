97aef5284780900524ac4677fd0c46b7dbbcd5a9
Felipe Mendes Fonseca <felipe.cwb13@gmail.com>
2022-11-24 18:20:16 -0300

    updating

---
+++ src/pages/SignIn.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { Signin} from './SignIn'
+
+export default{
+    title: 'Pages/Sign in',
+    component: SignIn,
+    args:{},
+    
+} as Meta
+
+export const Default: StoryObj = {}
